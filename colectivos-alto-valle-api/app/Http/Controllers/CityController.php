74730885582937f6ecdf0b8af8274ea04fcf55cf
Lucas Mendez-dev <lucas.mendez@est.fi.uncoma.edu.ar>
2023-07-26 00:39:39 -0300

    City endpoint added

--- colectivos-alto-valle-api/app/Http/Controllers/CityController.php
+++ colectivos-alto-valle-api/app/Http/Controllers/CityController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\City;
 
 class CityController extends Controller
 {
     //
+    public function index() {
+        return City::all();
+    }
 }
