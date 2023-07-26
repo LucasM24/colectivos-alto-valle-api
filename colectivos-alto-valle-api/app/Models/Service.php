2d3ad79ebd2b1a3d6a8f31ac507dfc1e927c90df
Lucas Mendez-dev <lucas.mendez@est.fi.uncoma.edu.ar>
2023-07-26 02:55:34 -0300

    Ulid added to cities and services

--- colectivos-alto-valle-api/app/Models/Service.php
+++ colectivos-alto-valle-api/app/Models/Service.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+    use HasUlids;
 
     public function setName(String $name): void
     {
--- colectivos-alto-valle-api/database/migrations/2023_06_21_020533_create_cities_table.php
+++ colectivos-alto-valle-api/database/migrations/2023_06_21_020533_create_cities_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('cities', function (Blueprint $table) {
-            $table->ulid('id');
+            $table->ulid('id')->primary();
             $table->string('name');
             $table->string('province');
             $table->timestamps();
